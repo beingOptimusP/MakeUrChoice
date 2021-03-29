cc8d0e468fca872a575eb50fd27485efb942d91d
vamsi <75676297+beingOptimusP@users.noreply.github.com>
2021-03-29 15:28:07 +0530

    models

--- polling/models.py
+++ polling/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class CreatPoll(models.Model):
     Title = models.TextField()
-    o1 = models.CharField( max_length=50)
-    o2 = models.CharField( max_length=50)
-    o3 = models.CharField( max_length=50)
-    o4 = models.CharField( max_length=50)
+    o1 = models.CharField( max_length=150)
+    o2 = models.CharField( max_length=150)
+    o3 = models.CharField( max_length=150)
+    o4 = models.CharField( max_length=150)
     on1= models.IntegerField(default=0)
     on2= models.IntegerField(default=0)
     on3= models.IntegerField(default=0)
